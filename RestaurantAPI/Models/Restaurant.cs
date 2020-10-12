663015228290f7ac4056e28040de3deef4ce844a
Nicholas Perry <nicholasperrypro@gmail.com>
2020-10-12 18:47:33 -0400

    Add project files.

---
+++ RestaurantAPI/Models/Restaurant.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RestaurantAPI.Models
+{
+    // Restaurant Entity
+    // Entity is just a business object we're storing in the database
+    public class Restaurant
+    {
+        [Key]
+        public int RestaurantId { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public string Hours { get; set; }
+        public string Address { get; set; }
+        [Required]
+        public string Cuisine { get; set; }
+    }
+}
